4e13afd7816ea86424a297abd27799b19326f0ec
ssaqua <ssaqua@users.noreply.github.com>
2019-08-28 21:00:48 +1200

    Update doc

--- app/src/sharedTest/java/ss/wallpad/TestApplication.kt
+++ app/src/sharedTest/java/ss/wallpad/TestApplication.kt
@@ -5,6 +5,7 @@ import android.app.Application
 /**
  * Use a separate [Application] to prevent initializing dependency injection.
  *
- * See [ss.wallpad.runner.WallPadTestRunner].
+ * See [ss.wallpad.runner.WallPadTestRunner] for instrumentation tests.
+ * Set with [org.robolectric.annotation.Config] annotation for Robolectric JVM tests.
  */
 class TestApplication : Application()
